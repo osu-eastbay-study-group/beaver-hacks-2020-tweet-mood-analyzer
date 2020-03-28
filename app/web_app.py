ccac4e4b334e9ed7393387d1c669ef586207acb5
Jeremy Tsang <tsangj@oregonstate.edu>
2020-03-28 11:15:06 -0700

    Add dummy data and testing template
    Add a pretend main status along with its children comment statuses.
    
    Use dynamic component in `search()' route.
    
    Use `render_template()' in `home()' and pass in dummy data.
    
    Create `home.html' as a template for view function `home()' and use the
    dummy data passed from `home()'. Show the main tweet in an `<h1>' and
    use a Jinja2 loop to place all the children tweets.

--- app/web_app.py
+++ app/web_app.py
@@ -1,19 +1,62 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+# List of colors
+tones = {
+    "anger",
+    "fear",
+    "joy",
+    "sadness",
+    "analytical",
+    "confident",
+    "tentative",
+    "none",
+}
+
+# Dummy Data
+main_tweet = {
+    'tweet_id': '1185017497482719233',
+    'tone': 'joy',
+    'intensity': '2',
+    'children': [
+        {
+            'tweet_id': '1185303393801310208',
+            'tone': 'joy',
+            'intensity': '1',
+        },
+        {
+            'tweet_id': '1185162876237029376',
+            'tone': 'joy',
+            'intensity': '1',
+        },
+        {
+            'tweet_id': '1185282630285062144',
+            'tone': 'joy',
+            'intensity': '1',
+        },
+        {
+            'tweet_id': '1185020345016827909',
+            'tone': 'joy',
+            'intensity': '1',
+        }
+    ]
+}
 
 
 @app.route("/<page_id>")
 def search(page_id):
     """Page to enter in Tweet link."""
-    return "<h1>Search</h1>"
+    return f"<h1>Page ID: {page_id}</h1>"
 
 
 @app.route("/")
 def home():
     """Home page."""
-    return "<h1>Home</h1>"
+    return render_template('home.html',
+                           main_tweet=main_tweet,
+                           children=main_tweet['children'])
     # return search("home")  # TODO: Make home be a special case of search().
 
 
