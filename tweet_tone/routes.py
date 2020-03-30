cd201c2bb704da28c8ea59ad625967d8574eb6da
Jeremy Tsang <tsangj@oregonstate.edu>
2020-03-29 17:15:45 -0700

    Move front end files into Flask app

--- tweet_tone/routes.py
+++ tweet_tone/routes.py
@@ -1,23 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from tweet_tone import app
 from tweet_tone.forms import TwitterLinkForm
 from tweet_tone.models import Tweet, Child
+import os
+
+IMG_FOLDER = os.path.join('static', 'img')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 
 
-@app.route("/<page_id>")
-def search(page_id):
+@app.route("/childtweet")
+def search():
     """Page to enter in Tweet link."""
-    return f"<h1>Page ID: {page_id}</h1>"
-
+    return render_template("childtweet.html")
 
 @app.route("/")
+@app.route("/home")
 def home():
-    """Home page."""
-    return render_template('home.html',
-                           main_tweet=main_tweet,
-                           children=main_tweet['children'])
-    # return search("home")  # TODO: Make home be a special case of search().
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'test.png')
+    return render_template("home.html", user_image = full_filename)
+    # return render_template('home.html')
